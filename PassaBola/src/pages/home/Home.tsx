96fc74400a4259563711b81f633aa38faeb693a1
Matheus Santos <Matheus.soliveira11@hotmail.com>
2025-09-10 18:01:20 -0300

    pagina home

--- PassaBola/src/pages/home/Home.tsx
+++ PassaBola/src/pages/home/Home.tsx
@@ -4,37 +4,37 @@ import casa from "../../imgs/home.png";
 import video from "../../imgs/video.png";
 import bola from "../../imgs/bola.png";
 import perfil from "../../imgs/perfil.png";
+import site from "../../imgs/selecaobrasileira.png";
 
 export default function Home() {
     return (
     <div className="min-h-screen flex flex-col bg-[#0c0c0c] text-white">
-        <header className="fixed top-0 w-full shadow-md z-2 bg-[#000000]">
+        <header className="w-full shadow-md z-2">
             <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
 
-                <div className="w-12" />
+                    <div className="w-12" />
 
-                <div className="flex-grow max-w-md mx-6">
-                    <input
-                    type="text"
-                    placeholder="Pesquisar..."
-                    className="w-full px-4 py-2 rounded-lg bg-white text-black placeholder-gray-400"
+                    <div className="flex-grow max-w-md mx-6">
+                        <input
+                            type="text"
+                            placeholder="Pesquisar..."
+                            className="w-full px-4 py-2 rounded-lg bg-white text-black placeholder-gray-400"
+                        />
+                    </div>
+
+                    <img
+                        src={logo}
+                        alt="Logo"
+                        className="h-12 w-12 object-contain"
                     />
-                </div>
-
-                <img
-                    src={logo}
-                    alt="Logo"
-                    className="h-12 w-12 object-contain"
-                />
 
             </div>
             <div className="w-full h-0.5 bg-[#ec4d9d]"></div>
         </header>
-        <div className="h-25"></div>
+
 
         {/*Conteúdo das página */}
-        
-        <div className="h-15"></div>
+
         <footer className="fixed bottom-0 w-full bg-[#000000] z-2">
             <div className="w-full h-0.5 bg-[#ec4d9d]"></div>
             <div className="flex gap-20 justify-center">
