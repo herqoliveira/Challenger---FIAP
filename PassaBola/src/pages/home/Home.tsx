e449fb8d921ac01042753433bba494322d41878f
Henrique de Oliveira <henrique2801oliveira@gmail.com>
2025-09-13 23:49:48 -0300

    Iniciando responsividade (trocando footer por aside)

--- PassaBola/src/pages/home/Home.tsx
+++ PassaBola/src/pages/home/Home.tsx
@@ -33,7 +33,7 @@ export default function Home() {
                             className="h-12 w-12 object-contain"
                         />
                     </Link>
-                    
+
 
                 </div>
                 <div className="w-full h-0.5 bg-[#ec4d9d]"></div>
@@ -46,7 +46,7 @@ export default function Home() {
                     <div className="bg-green-700 text-white w-90 rounded-lg shadow p-4">
                         <div className="flex justify-between items-center">
                             <p className="font-bold text-center">SANTOS 0 X 3 PALMEIRAS</p>
-                        </div>                        
+                        </div>
                         <p className="text-xs mt-2">🟥 Marcela 54'</p>
                         <p className="text-xs">⚽ Aninha 18' | Ruth 45+1' | Beatriz 79'</p>
                         <p className="text-yellow-300 text-xs mt-1">⏸️ Intervalo</p>
@@ -110,10 +110,11 @@ export default function Home() {
                     <h3 className="text-center">Carregando...</h3>
                 </div>
             </div>
-            
+
             {/*Conteúdo das página */}
-            <div className="h-10"></div>
-            <footer className="fixed bottom-0 w-full bg-[#000000] z-2">
+            <div className="h-10 lg:hidden"></div>
+
+            <footer className="fixed bottom-0 w-full bg-[#000000] z-2 lg:hidden">
                 <div className="w-full h-0.5 bg-[#ec4d9d]"></div>
                 <div className="flex gap-20 justify-center">
                     <Link to="/home">
@@ -130,6 +131,35 @@ export default function Home() {
                     </Link>
                 </div>
             </footer>
+            <aside className="fixed z-1 w-35 h-full bg-black flex-col items-center py-4 hidden lg:flex">
+                <div className="mt-40">
+                    <Link to="/home">
+                        <div className="flex">
+                            <img src={casa} alt="Home" className="w-10 h-10 mb-6" />
+                            <p className="mt-2.5 ml-1 font-bold">Home</p>
+                        </div>
+                    </Link>
+                    <Link to="/videos">
+                        <div className="flex">
+                            <img src={video} alt="Vídeos" className="w-10 h-10 mb-6" />
+                            <p className="mt-2.5 ml-1">Vídeos</p>
+                        </div>
+                    </Link>
+                    <Link to="/jogos">
+                        <div className="flex">
+                            <img src={bola} alt="Jogos" className="w-10 h-10 mb-6" />
+                            <p className="mt-2.5 ml-1">Jogos</p>
+                        </div>
+                    </Link>
+                    <Link to="/perfil3">
+                        <div className="flex">
+                            <img src={perfil} alt="Perfil" className="w-10 h-10" />
+                            <p className="mt-2.5 ml-1">Perfil</p>
+                        </div>
+                    </Link>
+                </div>
+            </aside>
+
         </div>
     );
 }
--- PassaBola/src/pages/videos/Videos.tsx
+++ PassaBola/src/pages/videos/Videos.tsx
@@ -85,11 +85,11 @@ export default function Videos() {
                         <img src={video4} alt="" className="p-4 w-90 h-50 rounded-lg" />
                     </div>
                 </div>
-                <h3>Carregando...</h3>
+                <h3 className="lg:mb-4">Carregando...</h3>
             </div>
-            <div className="h-15"></div>
+            <div className="h-15 lg:hidden"></div>
 
-            <footer className="fixed bottom-0 w-full bg-[#000000] z-2">
+            <footer className="fixed bottom-0 w-full bg-[#000000] z-2 lg:hidden">
                 <div className="w-full h-0.5 bg-[#ec4d9d]"></div>
                 <div className="flex gap-20 justify-center">
                     <Link to="/home">
@@ -106,6 +106,34 @@ export default function Videos() {
                     </Link>
                 </div>
             </footer>
+            <aside className="fixed z-1 w-35 h-full bg-black flex-col items-center py-4 hidden lg:flex">
+                <div className="mt-40">
+                    <Link to="/home">
+                        <div className="flex">
+                            <img src={casa} alt="Home" className="w-10 h-10 mb-6" />
+                            <p className="mt-2.5 ml-1 ">Home</p>
+                        </div>
+                    </Link>
+                    <Link to="/videos">
+                        <div className="flex">
+                            <img src={video} alt="Vídeos" className="w-10 h-10 mb-6" />
+                            <p className="mt-2.5 ml-1 font-bold">Vídeos</p>
+                        </div>  
+                    </Link>
+                    <Link to="/jogos">
+                        <div className="flex">
+                            <img src={bola} alt="Jogos" className="w-10 h-10 mb-6" />
+                            <p className="mt-2.5 ml-1">Jogos</p>
+                        </div>
+                    </Link>
+                    <Link to="/perfil3">
+                        <div className="flex">
+                            <img src={perfil} alt="Perfil" className="w-10 h-10" />
+                            <p className="mt-2.5 ml-1">Perfil</p>
+                        </div>
+                    </Link>
+                </div>
+            </aside>
         </div>
     );
 }
