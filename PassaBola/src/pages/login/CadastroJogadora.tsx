cbc5bf61c86a4b4440730bc5c05df1f3d1e57479
Henrique <henrique2801oliveira@gmail.com>
2025-11-04 16:52:47 -0300

    Creating CadastroJogadora.tsx

--- PassaBola/src/pages/login/CadastroJogadora.tsx
+++ PassaBola/src/pages/login/CadastroJogadora.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { POSICOES, carregarJogadoras, salvarJogadoras } from "../../data/jogadoras";
 import type { Jogadora } from "../../data/jogadoras";
 import { v4 as uuidv4 } from "uuid";
-
+import wallpaper from "../../imgs/cadastro.jpg";
 export default function CadastroJogadora() {
   const [nome, setNome] = useState("");
   const [nome_usu, setNome_usu] = useState("");
@@ -51,113 +51,134 @@ export default function CadastroJogadora() {
   };
 
   return (
-    <div className="min-h-screen bg-[#0c0c0c] text-white flex flex-col items-center py-10">
-      <h1 className="text-2xl font-bold mb-6">Cadastrar Nova Jogadora</h1>
+    <div className="h-full flex flex-col items-center py-10 bg-cover " style={{ backgroundImage: `url(${wallpaper})` }}>
+      <h1 className="text-2xl font-bold mb-6 text-white">Cadastrar Nova Jogadora</h1>
 
-      <div className="flex flex-col gap-4 w-80 bg-black p-6 rounded-2xl shadow-lg">
-        <label>
-          Nome:
-          <input
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            type="text"
-            className="w-full mt-1 p-2 rounded bg-gray-800 text-white"
-          />
-        </label>
+      <div className="flex flex-col gap-4 w-80 bg-black p-6 rounded-2xl shadow-lg lg:w-175">
+        <div className="lg:flex lg:flex-wrap">
+          <div className="lg:w-80 text-white">
+            <label>
+              Nome:
+              <br />
+              <input
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+                type="text"
+                placeholder="Digite seu nome"
+                className="w-68 mt-1 p-2 rounded bg-[#2e2e2e] text-white"
+              />
+            </label>
+            <br />
+            <label>
+              Nome de usuária:
+              <input
+                value={nome}
+                onChange={(e) => setNome_usu(e.target.value)}
+                type="text"
+                placeholder="ex: @marta1234"
+                className="w-68 mt-1 p-2 rounded bg-[#2e2e2e] text-white"
+              />
+            </label>
+            <br />
+            <label>
+              Idade:
+              <br />
+              <input
+                value={idade}
+                onChange={(e) => setIdade(Number(e.target.value))}
+                type="number"
+                placeholder="Digite sua idade"
+                className="w-68 mt-1 p-2 rounded bg-[#2e2e2e] text-white"
+              />
+            </label>
 
-        <label>
-          Nome de usuária:
-          <input
-            value={nome}
-            onChange={(e) => setNome_usu(e.target.value)}
-            type="text"
-            placeholder="ex: @marta1234"
-            className="w-full mt-1 p-2 rounded bg-gray-800 text-white"
-          />
-        </label>
+            <label>
+              Posição:
+              <br />
+              <select
+                value={posicao}
+                onChange={(e) => setPosicao(e.target.value)}
+                className="w-68 mt-1 p-2 rounded bg-[#2e2e2e] text-white"
+              >
+                <option value="">Selecione...</option>
+                {POSICOES.map((p) => (
+                  <option key={p} value={p}>
+                    {p}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <br />
+            <label>
+              Time do coração:
+              <br />
+              <input
+                value={time}
+                onChange={(e) => setTime(e.target.value)}
+                type="text"
+                placeholder="Digite seu time do coração"
+                className="w-68 mt-1 p-2 rounded bg-[#2e2e2e] text-white"
+              />
+            </label>
+            <br />
+          </div>
+          <div className="lg:w-80 text-white">
+            <label>
+              Email:
+              <br />
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="Digite seu email"
+                className="w-68 mt-1 p-2 rounded bg-[#2e2e2e] text-white"
+              />
+            </label>
+            <br />
+            <label>
+              Senha:
+              <br />
+              <input
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+                type="password"
+                placeholder="Digite uma senha"
+                className="w-68 mt-1 p-2 rounded bg-[#2e2e2e] text-white"
+              />
+            </label>
+            <br />
+            <div className="h-0.5 w-68 bg-gray-900 mt-3 mb-2 "></div>
+            <label>
+              Foto de perfil:
+              <br />
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleFotoChange}
+                className="w-68 mt-1 text-sm"
+              />
+            </label>
 
-        <label>
-          Idade:
-          <input
-            value={idade}
-            onChange={(e) => setIdade(Number(e.target.value))}
-            type="number"
-            className="w-full mt-1 p-2 rounded bg-gray-800 text-white"
-          />
-        </label>
+            {foto && (
+              <img
+                src={foto}
+                alt="Pré-visualização"
+                className="w-24 h-24 object-cover rounded-full mx-auto"
+              />
+            )}
 
-        <label>
-          Posição:
-          <select
-            value={posicao}
-            onChange={(e) => setPosicao(e.target.value)}
-            className="w-full mt-1 p-2 rounded bg-gray-800 text-white"
-          >
-            <option value="">Selecione...</option>
-            {POSICOES.map((p) => (
-              <option key={p} value={p}>
-                {p}
-              </option>
-            ))}
-          </select>
-        </label>
+            <button
+              onClick={handleSalvar}
+              className="bg-pink-600 hover:bg-pink-700 transition-all p-2 rounded font-bold mt-1 text-white"
+            >
+              Salvar Jogadora
+            </button>
 
-        <label>
-          Time do coração:
-          <input
-            value={time}
-            onChange={(e) => setTime(e.target.value)}
-            type="text"
-            className="w-full mt-1 p-2 rounded bg-gray-800 text-white"
-          />
-        </label>
+            {mensagem && <p className="text-center text-sm mt-2">{mensagem}</p>}
+          </div>
+        </div>
 
-        <label>
-          Email:
-          <input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="email"
-            className="w-full mt-1 p-2 rounded bg-gray-800 text-white"
-          />
-        </label>
 
-        <label>
-          Senha:
-          <input
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-            type="password"
-            className="w-full mt-1 p-2 rounded bg-gray-800 text-white"
-          />
-        </label>
-
-        <label>
-          Foto de perfil:
-          <input
-            type="file"
-            accept="image/*"
-            onChange={handleFotoChange}
-            className="w-full mt-1 text-sm"
-          />
-        </label>
-
-        {foto && (
-          <img
-            src={foto}
-            alt="Pré-visualização"
-            className="w-24 h-24 object-cover rounded-full mx-auto"
-          />
-        )}
-
-        <button
-          onClick={handleSalvar}
-          className="bg-pink-600 hover:bg-pink-700 transition-all p-2 rounded font-bold"
-        >
-          Salvar Jogadora
-        </button>
-
-        {mensagem && <p className="text-center text-sm mt-2">{mensagem}</p>}
       </div>
     </div>
   );
